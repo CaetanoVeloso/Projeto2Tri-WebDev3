04781a27420769dcd6b80c691b4b57583d92510b
CaetanoVl <caetanoemanuel@yahoo.com>
2023-08-05 12:17:40 -0300

    Criação da homepage inicial

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Projeto 2Tri - WebDev3</title>
+</head>
+
+<body>
+    <header>
+        <h2>Desenvolvimento Web</h2>
+        <a href="#" class="home"> Home </a>
+    </header>
+
+    <main>
+        <h1>Trabalho: Questões</h1>
+        <ul>
+            <li><a href="pages\q1\q1.php">Questão 1</a></li>
+            <li><a href="pages\q1\q2.php">Questão 2</a></li>
+            <li><a href="pages\q1\q3.php">Questão 3</a></li>
+            <li><a href="pages\q1\q4.php">Questão 4</a></li>
+        </ul>
+    </main>
+
+    <footer>
+        <p>Caetano Veloso e Tales Cordeiro - &copy;2023</p>
+    </footer>
+
+</body>
+
+</html>
